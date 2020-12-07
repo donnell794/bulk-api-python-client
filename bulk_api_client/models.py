bec99b118a61acdbfd18f476de03a469047c5204
Mikela Clemmons <glassresistor@gmail.com>
2020-12-07 14:55:14 -0800

    removed snake case stuff

--- bulk_api_client/models.py
+++ bulk_api_client/models.py
@@ -7,10 +7,6 @@ models = sys.modules[__name__]
 __all__ = []
 
 
-def _snake_to_camel(name):
-    return re.sub(r"(?:^|_)([a-z])", lambda x: x.group(1).upper(), name)
-
-
 class App:
     def __init__(self, app_name):
         self.app = env_client.app(app_name)
@@ -18,7 +14,7 @@ class App:
         __all__.append(app_name)
 
     def add_model(self, model_name):
-        setattr(self, _snake_to_camel(model_name), self.app.model(model_name))
+        setattr(self, model_name.capitalize(), self.app.model(model_name))
 
 
 for definition in env_client.definitions.keys():
--- tests/models_tests.py
+++ tests/models_tests.py
@@ -9,9 +9,9 @@ from bulk_api_client import client
 def test_models():
     FakeClient = mock.MagicMock()
     FakeClient.definitions = {
-        "app1.model_one": {},
-        "app1.model_two": {},
-        "app2.model_three": {},
+        "app1.modelone": {},
+        "app1.modeltwo": {},
+        "app2.modelthree": {},
     }
     FakeApp1 = mock.MagicMock()
     FakeApp2 = mock.MagicMock()
@@ -38,15 +38,15 @@ def test_models():
         [call("app1"), call("app2"),]
     )
     FakeApp1.model.assert_has_calls(
-        [call("model_one"), call("model_two"),]
+        [call("modelone"), call("modeltwo"),]
     )
     FakeApp2.model.assert_has_calls(
-        [call("model_three"),]
+        [call("modelthree"),]
     )
     assert all(
         [
-            models.app1.ModelOne == FakeModel1,
-            models.app1.ModelTwo == FakeModel2,
-            models.app2.ModelThree == FakeModel3,
+            models.app1.Modelone == FakeModel1,
+            models.app1.Modeltwo == FakeModel2,
+            models.app2.Modelthree == FakeModel3,
         ]
     )
